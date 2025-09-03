d30bfb02f176db2ed166dbdea5e8792c0733f66e
srivastava-aryan <aru93295@gmail.com>
2025-09-03 14:30:02 +0530

    added origin

--- project-server/server.js
+++ project-server/server.js
@@ -5,7 +5,7 @@ const mailRoutes = require("./routes/mailRoutes");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://aryan-srivastava-08.vercel.app"], credentials: true }));
 app.use(express.json());
 
 // Routes
